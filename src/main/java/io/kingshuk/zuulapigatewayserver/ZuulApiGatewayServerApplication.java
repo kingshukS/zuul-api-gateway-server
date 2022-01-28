16c887c98a71b218c4ff954c472a017a3cfbd967
Kingshuk Saha <kingshuk1808@gmail.com>
2022-01-28 23:48:54 +0530

    initial commit

---
+++ src/main/java/io/kingshuk/zuulapigatewayserver/ZuulApiGatewayServerApplication.java
@@ -0,0 +1,23 @@
+package io.kingshuk.zuulapigatewayserver;
+
+import brave.sampler.Sampler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayServerApplication.class, args);
+	}
+
+	@Bean
+	public Sampler sampler(){
+		return Sampler.ALWAYS_SAMPLE;
+	}
+}
